ab5a94a6785179d2e91bac13eab1fe9f55466f3e
Kris Councell <kcouncell7215@vbtc.com>
2017-03-20 10:27:20 -0400

    initial coding for mixUp()

--- Helllo/Helllo/Helllo.cpp
+++ Helllo/Helllo/Helllo.cpp
@@ -12,3 +12,17 @@ int main()
 	return 0;
 }
 
+string mixUp(const string& aWord)
+{
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
+}
