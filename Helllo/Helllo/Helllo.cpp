d125d2164c311250556fe68684b83e42e00c0e39
Connor Viney <cviney0418@vbtc.net>
2017-03-20 09:32:04 -0400

    init commit

---
+++ Helllo/Helllo/Helllo.cpp
@@ -0,0 +1,14 @@
+// Helllo.cpp : Defines the entry point for the console application.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	system("Pause");
+	return 0;
+}
+
