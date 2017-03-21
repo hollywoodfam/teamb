62ec5f35ed4fc3a048df36a733cbd6f6261de8f1
Trace Snyder <tsnyder7856@vbtc.net>
2017-03-21 09:29:28 -0400

    Play

--- Helllo/Helllo/Helllo.cpp
+++ Helllo/Helllo/Helllo.cpp
@@ -1,14 +1,19 @@
-// Helllo.cpp : Defines the entry point for the console application.
-//
-
-
-#include <iostream>
-using namespace std;
-
-int main()
+void play(const string& aWord)
 {
-	cout << "Hello World" << endl;
-	system("Pause");
-	return 0;
-}
+	string guess;
+	do
+	{
+		cout << endl << endl << "Your guess: ";
+		cin >> guess;
 
+		if ((guess != aWord) && (guess != "quit"))
+		{
+			cout << "Sorry, that's not it.";
+		}
+	} while ((guess != aWord) && (guess != "quit"))
+
+	if (guess == aWord)
+		cout << endl << "That's it! You Guessed it!" << endl;
+
+	cout << endl << "Thanks for playing.\n" << endl;
+}
