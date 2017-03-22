2c07871cd80e18d854f7de6c64f5d4418adc7bae
nilloc <21collin.bennett@ppps.org>
2017-03-22 09:36:28 -0400

    really new cpp

--- Helllo/Helllo/Helllo.cpp
+++ Helllo/Helllo/Helllo.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
 void welcomePlayer(const string& aJumble)
 {
 	cout << "\t\t\Welcome to Word Jumble!" << endl << endl;
 	cout << "Unscramble the letter to make a word." << endl;
 	cout << "Enter 'quit' to quit the game." << endl;
-	cout << "The jumble is: " << aJUmble;
+	cout << "The jumble is: " << aJumble;
 }
