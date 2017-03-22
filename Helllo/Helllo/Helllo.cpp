5a119da6fca0cfdeffbd1db0499f79fb29f4e3e5
nilloc <21collin.bennett@ppps.org>
2017-03-22 10:04:03 -0400

    comment

--- Helllo/Helllo/Helllo.cpp
+++ Helllo/Helllo/Helllo.cpp
@@ -3,12 +3,49 @@
 
 
 #include <iostream>
+#include <string>
+//#include <cstdlib>
+#include <time.h>
+
 using namespace std;
+
+string randomWord();
+string mixUp(const string& aWord);
+void welcomePlayer(const string& aJumble);
+void play(const string& aWord);
 
 int main()
 {
-	cout << "Hello World" << endl;
-	system("Pause");
+	srand(static_cast<unsigned int>(time(0)));
+
+	string word = randomWord();
+	string jumble = mixUp(word);
+	welcomePlayer(jumble);
+	play(word);
+
+	system("pause");
+
 	return 0;
+}
+
+
+string randomWord()
+{
+	return string();
+}
+
+string mixUp(const string& aWord)
+{
+	return string();
+}
+
+void welcomePlayer(const string& aJumble)
+{
+
+}
+
+void play(const string& aWord)
+{
+
 }
 
