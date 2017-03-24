9f42cee75d7bcf626e2cbb595896a1305dbddd04
0ConnorMoss0 <mrbritishguy1@gmail.com>
2017-03-24 08:42:06 -0400

    Update Helllo.cpp

--- Helllo/Helllo/Helllo.cpp
+++ Helllo/Helllo/Helllo.cpp
@@ -2,13 +2,20 @@
 //
 
 
-#include <iostream>
-using namespace std;
-
-int main()
+string randomWord()
 {
-	cout << "Hello World" << endl;
-	system("Pause");
-	return 0;
-}
+	const int NUM_WORDS = 5;
+	string WORDS[NUM_WORDS] =
+	{
 
+		"green",
+		"blue",
+		"waffle",
+		"pancake",
+		"bacon"
+	};
+
+	init choice = (rand() % NUM_WORDS);
+	string word = WORDS[choice];
+	return word;
+}
